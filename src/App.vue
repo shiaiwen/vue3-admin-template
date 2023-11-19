4f9a0d56a305df6272e5c83f1be92b4ebb5734cb
shiaiwen <2474577454@qq.com>
2023-11-19 19:35:42 +0800

    feat: 添加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <h1>我是APP根组件</h1>
+  <h1>标题</h1>
 </template>
 
 <script setup lang="ts">
-  const str = '我爱你中国'
+const str = '我爱你中国'
 
-  for (var i=0; i<1000; i++) {
-    
-
-
-    console.log(
-      '格式化测试'
-    )
-  }
+for (var i = 0; i < 1000; i++) {
+  console.log('格式化测试')
+}
 </script>
